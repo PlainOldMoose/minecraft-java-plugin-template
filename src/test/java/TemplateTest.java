b326b74673a71546697f297a9c2e535da45e89a5
Luque <luque@vandermerwe.uk>
2025-10-05 20:32:29 +0100

    Updated template

---
+++ src/test/java/TemplateTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TemplateTest {
+    @BeforeEach
+    void setup() {
+
+    }
+
+    @Test
+    void test1() {
+
+    }
+}
